6833d33d40d494040eff6c214cc5bc7d5964992e
Jack <37254717+jackbullen@users.noreply.github.com>
2023-02-26 08:41:13 -0800

    Fix

--- island_chess/settings.py
+++ island_chess/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'SECRET_KEY_GOES_HERE'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
